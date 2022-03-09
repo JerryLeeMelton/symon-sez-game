3eabe921d2bbefc658375faf0ef95580fbfa9c2b
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-03-09 07:43:06 -0800

    Add KUTE.js animation to background

---
+++ public/scripts/animation.js
@@ -0,0 +1,34 @@
+const st1tween = KUTE.fromTo(
+  "#st1-a",
+  {path: "#st1-a"},
+  {path: "#st1-b"},
+  {repeat: 999, duration: 3000, yoyo: true}
+).start();
+
+const st2tween = KUTE.fromTo(
+  "#st2-a",
+  {path: "#st2-a"},
+  {path: "#st2-b"},
+  {repeat: 999, duration: 3000, yoyo: true}
+).start();
+
+const st3tween = KUTE.fromTo(
+  "#st3-a",
+  {path: "#st3-a"},
+  {path: "#st3-b"},
+  {repeat: 999, duration: 3000, yoyo: true}
+).start();
+
+const st4tween = KUTE.fromTo(
+  "#st4-a",
+  {path: "#st4-a"},
+  {path: "#st4-b"},
+  {repeat: 999, duration: 3000, yoyo: true}
+).start();
+
+const st5tween = KUTE.fromTo(
+  "#st5-a",
+  {path: "#st5-a"},
+  {path: "#st5-b"},
+  {repeat: 999, duration: 3000, yoyo: true}
+).start();
