22911fd2be29d55fc58da6314f58fee3c59e0e98
Jerry Lee Melton <jerryleemelton@gmail.com>
2022-03-15 11:55:55 -0700

    Add score counter to code

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -5,6 +5,7 @@ let userPattern = [];
 let gameRunning = false;
 let allowButtonClicks = false;
 let delayInterval = 500;
+let score = 0;
 
 let gameButtons = $(".game-button");
 let startButton = $("#StartButtonInner-Lit");
@@ -24,20 +25,20 @@ function nextSequence() {
 }
 
 function playSequence() {
-  var promise = Promise.resolve();
+  return new Promise((resolve) => {
+    var promise = Promise.resolve();
 
-  // THIS PROMISE CODE IS IMPORTANT
-  gamePattern.forEach(function(currentValue, index) {
-    promise = promise.then(function() {
-      animateButton(currentValue + "Button-Lit", "play");
-      playSound(currentValue);
-      return new Promise(function(resolve) {
-        setTimeout(resolve, delayInterval);
-      })
+    gamePattern.forEach(function(currentValue, index) {
+      promise = promise.then(function() {
+        animateButton(currentValue + "Button-Lit", "play");
+        playSound(currentValue);
+        return new Promise(function(resolve) {
+          setTimeout(resolve, delayInterval);
+        })
+      });
     });
-  });
-
-  allowButtonClicks = true;
+    resolve();
+  })
 }
 
 function playSound(sound) {
@@ -59,7 +60,23 @@ function animateButton(button) {
       opacity: "0"
     }, animationDuration, "swing")
   }, 100);
+}
 
+function toggleButtonClicksAllowed() {
+  allowButtonClicks = !allowButtonClicks;
+}
+
+function lightAllButtons() {
+  let animationDuration = 50;
+  gameButtons.animate({
+    opacity: 1.0
+  }, animationDuration, "swing");
+
+  setTimeout(() => {
+    gameButtons.animate({
+      opacity: "0"
+    }, animationDuration, "swing")
+  }, 1000);
 }
 
 // Game Logic =================================================================
@@ -69,25 +86,27 @@ gameButtons.on("click", (event) => {
   if (!gameRunning && !allowButtonClicks) return;
 
   allowButtonClicks = false;
-  animateButton(event.target.id);
   userPattern.push(event.target.id.replace("Button-Lit", ""));
 
   if (userPattern[userPattern.length - 1] !== gamePattern[userPattern.length - 1]) {
     playSound("wrong");
+    lightAllButtons();
     gameRunning = false;
     return;
   }
 
+  animateButton(event.target.id);
   playSound(event.target.id.replace("Button-Lit", ""));
 
-  if (userPattern.length !== gamePattern.length) {
+  if (userPattern.length !<= gamePattern.length) {
     allowButtonClicks = true;
     return;
   }
 
+  score++;
   nextSequence();
   setTimeout(function() {
-    playSequence();
+    playSequence().then(toggleButtonClicksAllowed());
   }, delayInterval + delayInterval);
 });
 
@@ -95,6 +114,7 @@ gameButtons.on("click", (event) => {
 startButton.on("click", (event) => {
   if (!gameRunning) {
     animateButton(event.target.id);
+    score = 0;
     startGame();
   }
 });
